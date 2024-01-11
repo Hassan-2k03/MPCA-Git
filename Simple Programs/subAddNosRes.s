2e097fd9f47a9cce14d9de3e5aedda337dccf8f6
Mohammed Hassan <mh1921544@gmail.com>
2024-01-11 10:12:25 +0530

    Initial commit

---
+++ Simple Programs/subAddNosRes.s
@@ -0,0 +1,14 @@
+
+;Add 2 numbers loaded from register
+.text
+Mov R0, #10
+Mov R1, #20
+Add R2, R0, R1
+
+
+;Sub 2 numbers loaded from register
+.text
+Mov R0, #10
+Mov R1, #20
+Sub R2, R0, R1
+.end
