9c5e7a0aa0a5a67be86d9a5f18e0e010e1282566
Mohammed Hassan <mh1921544@gmail.com>
2024-01-24 11:36:59 +0530

    Refactor prog05i.s to use .byte instead of .word for storing data

--- Lab 02/prog05i.s
+++ Lab 02/prog05i.s
@@ -1,2 +1,5 @@
-Write a program to add array of ten 8-bit numbers taking data from memory location stored as byte data (use .byte to store the data
-    instead of .word)
+/* Write a program to add array of ten 8-bit numbers taking data from memory location stored as byte data (use .byte to store the data 
+instead of .word) */
+
+@code 
+
