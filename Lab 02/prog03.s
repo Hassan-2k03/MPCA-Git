bdecbfdd8244bd02b7e30405bd569706b7d091cc
Mohammed Hassan <mh1921544@gmail.com>
2024-01-24 11:00:59 +0530

    Refactor code to classify and find the largest number

--- Lab 02/prog03.s
+++ Lab 02/prog03.s
@@ -1,2 +1,41 @@
-Write a program to Classify the given set of numbers as positive, negative or zero and also store them in different memory locations.
+/* Write a program to Classify the given set of numbers as positive, negative or zero and also store them in different memory locations.
+
+Algorithm
+
+Declare the array A in .data segment
+Let R0 contain address of A,R1 contain the address of array pos,R2 contain the address of array neg,R3 contain theaddress of array zero
+Let R4 contain the count for number of elements in array
+Copy the first element of array to a temporary register R8
+Compare element with zero
+If element greater than zero, save the value in array pos.Increment R1 to point to next location of pos.Increment R0 to continue to check for remaining array elements.
+Decrement R4 by 1(number of elements in array)
+Repeat step 6a till all elements of array are checked.If R4 equals to zero go to end of program
+
+OR
+
+If element lesser than zero, save the value in array neg.Increment R2 to point to next location of neg.Increment R0 to continue to check for remaining array elements.
+Decrement R4 by 1(number of elements in array)
+Repeat step 6b till R4 is not equal to zero.If R4 equals to zero go to end of program
+
+OR
+
+If element is equal zero, save the value in array zero.Increment R3 to point to next location of zero.Increment R0 tocontinue to check for remaining array elements.
+Decrement R4 by 1(number of elements in array)
+Repeat step 6c till R4 is not equal to zero.If R4 equals to zero go to end of program */
+
+@ A: .word 10,-3,41,-55,30,22,0,5,83,0
+
+@ pos
+
+@ 10,41,30,22,5,83
+
+@ neg
+
+@ -3,-55
+
+@ zero
+
+@ 0,0
+
+@ code:
 
--- Lab 02/prog04.s
+++ Lab 02/prog04.s
@@ -1,1 +1,39 @@
 Write a program to find the largest number from a given set of numbers.
+
+Algorithm
+
+1.Initialise array A in .data segment
+
+2.Let R0 contain address of A
+
+3.Copy the first element of array A to R1.
+
+Initial value of biggest number is assumed to be 10 stored in R1
+
+4.Let R3 contain count of number of elements in array A
+
+5.Compare each element of array A with biggest value in R1
+
+6.If R1 is less than current element of array A ,then current element
+
+of array A becomes new BIG value in R1
+
+7.Check for all elements of array A
+
+8.Decrement R3 by 1(Count for number of elements of array A)
+
+9.Repeat checking all the array elements until R3 becomes zero
+
+10. If R1 is greater than current element of array A,continue to check for remaining
+elements of array A
+
+11.Decrement R3 by 1(Count for number of elements of array A)
+
+12.Repeat checking all the array elements until R3 becomes zero
+
+A: .word
+10,50,41,55,30,20,11,
+5,100,77
+
+Largest
+value=100=0X64
