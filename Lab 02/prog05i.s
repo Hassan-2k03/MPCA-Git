5a50bfcf67c2fec0764a3d8745500e20fed31cbf
Mohammed Hassan <mh1921544@gmail.com>
2024-01-26 08:03:37 +0530

    Add code to calculate sum of array elements

--- Lab 02/prog05i.s
+++ Lab 02/prog05i.s
@@ -3,3 +3,22 @@ instead of .word) */
 
 @code 
 
+.data
+    array: .byte 1,2,3,4,5,6,7,8,9,10
+    sum: .byte 0
+
+.text
+    .globl main
+main:
+    ldr r0,=array
+    mov r1,#0
+    mov r2,#10
+loop:
+    ldrb r3,[r0],#1
+    add r1,r1,r3
+    subs r2,r2,#1
+    bne loop
+    strb r1,[r0]
+    ldr r0,=sum
+    ldrb r0,[r0]
+.end
