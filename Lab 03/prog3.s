f9e36fb5ededce333ca8cb2c9b9476ea57698d73
Mohammed Hassan <mh1921544@gmail.com>
2024-01-31 11:58:04 +0530

    Update prog3.s: Initialize n and calculate sum

--- Lab 03/prog3.s
+++ Lab 03/prog3.s
@@ -2,8 +2,19 @@
 @code: 
 
 .data
-    n:.word 0xffffffff
+    n: .word 0x12345678
+    sum: .word 0
 .text
-    ldr r0,=n
-    ldr r0,[r0]
-    
+    ldr r0, =n
+    ldr r1, [r0]
+    ldr r0, =sum
+    mov r2, #0
+loop:
+    mov r3, r1
+    and r3 ,r3, #0xf
+    add r2, r2, r3
+    mov r1, r1, lsr #4
+    cmp r1, #0
+    bne loop
+    str r2, [r0]
+    swi 0x11
