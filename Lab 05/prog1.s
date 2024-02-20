a5be581ec17d0f65d1ff1cb2bad05f54fd4d044f
Mohammed Hassan <mh1921544@gmail.com>
2024-02-20 11:33:50 +0530

    Update prog1.s

--- Lab 05/prog1.s
+++ Lab 05/prog1.s
@@ -10,18 +10,14 @@
     ldr r2,=c   ;result
     mov r7,#3   ;no of rows
     mov r9,#3   ;no of cols
-    loop2:
+loop2:
     mov r6,r0   ;cpy of r0
     mov r5,r1   ;cpy of r1
-    mov r8,#0   ;sum
-    loop1:
-    ldr r10,[r6],#4 ;load matrix 1
-    ldr r11,[r5],#12 ;load matrix 2
+    
+loop1:
+    ldr r10,[r6],#4 ;load matrix a
+    ldr r11,[r5],#12 ;load matrix b
     mla r8,r10,r11,r8 ;multiply and add
-    subs r9,r9,#1 ;decrement cols
+    subs r7,r7,#1 ;decrement rows in r4
     bne loop1
-    str r8,[r2],#4 ;store result
-    add r0,r0,#4 ;next row
-    mov r9,#3 ;reset cols
-    subs r7,r7,#1 ;decrement rows
-    bne loop2
+    
